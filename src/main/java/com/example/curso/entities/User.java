ee653c3be8cb8554c31419f28a154e18fe343e36
Paulo Arthur Rocha <paulo.arthu@hotmail.com>
2020-10-26 16:48:32 -0300

    Pedidos, Data, ISO 8601

--- src/main/java/com/example/curso/entities/User.java
+++ src/main/java/com/example/curso/entities/User.java
@@ -1,17 +1,24 @@
 package com.example.curso.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -19,9 +26,17 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String passaword;
-	
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 	public User() {
-		
+
 	}
 
 	public User(Long id, String name, String email, String phone, String passaword) {
@@ -93,7 +108,5 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
 
 }
