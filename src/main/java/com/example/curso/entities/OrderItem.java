9872a767198e4aac307df1d0e235aad23bc06d29
Paulo Arthur Rocha <paulo.arthu@hotmail.com>
2020-10-28 17:49:07 -0300

    OrderItem, ManyToOne e associaçoes - CORREÇAO

--- src/main/java/com/example/curso/entities/OrderItem.java
+++ src/main/java/com/example/curso/entities/OrderItem.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import com.example.curso.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_order_item")
@@ -32,6 +33,7 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
