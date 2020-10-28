2c4ba15e17d67818ab9f866fadc1a2db27aeedb5
Paulo Arthur Rocha <paulo.arthu@hotmail.com>
2020-10-28 20:35:01 -0300

    User update

--- src/main/java/com/example/curso/Resource/UserResource.java
+++ src/main/java/com/example/curso/Resource/UserResource.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,8 @@ import com.example.curso.services.UserService;
 
 import java.net.URI;
 import java.util.List;
+
+import javax.xml.ws.Response;
 
 @RestController
 @RequestMapping(value = "/Users")
@@ -50,4 +53,11 @@ public class UserResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+
 }
